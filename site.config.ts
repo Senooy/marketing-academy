be9ee2f5738f925ecfe054d507a269df2593ec2e
Dokgo <nskhelifi@gmail.com>
2024-01-04 20:25:27 +0100

    yarn deployment bug

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Marketing Academy, votre boussole dans le monde du marketing digital',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Younes Khelifi',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
@@ -53,13 +53,13 @@ export default siteConfig({
   navigationStyle: 'default',
   // navigationStyle: 'custom',
   navigationLinks: [
-     {
-       title: 'About',
-       pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-     },
+    {
+      title: 'About',
+      pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+    },
     {
       title: 'Contact',
-     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+      pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
     }
   ]
 })
